e6a7674aade9c16884f400507614649ba980ab5d
jyunyia@gmail.com <jyunyia@gmail.com>
2025-11-18 23:28:27 +0800

    week1

--- week01-2b.py
+++
@@ -1,4 +0,0 @@
-///week01-2b
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-       return haystack.find(needle)
---
+++ week1/week01-2b.py
@@ -0,0 +1,4 @@
+///week01-2b
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+       return haystack.find(needle)
