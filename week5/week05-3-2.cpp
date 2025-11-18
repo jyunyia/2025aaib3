7c312547af2524a5cc13548197e1b7ed58d81cab
jyunyia@gmail.com <jyunyia@gmail.com>
2025-11-18 23:32:59 +0800

    week5

---
+++ week5/week05-3-2.cpp
@@ -0,0 +1,22 @@
+///week05-3b.cpp 三合一 part1/part2
+///CPE
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string line;///part 1:Input
+    while(getline(cin, line)){///讀入1行
+        stringstream ss(line);///part3
+        string word;
+    while( ss >> word ){///part3
+        reverse( word.begin(), word.end() );
+        cout << " " <<word;
+    }
+        ///port 2:Output
+        cout << endl;///先隨便印出來
+    }
+}
+
